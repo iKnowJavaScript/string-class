ba4efaca0ea9b8d9f8f21e1eb018c0e51a601606
iKnowJavaScript <remymartins0398@yahoo.com>
2019-05-06 15:33:29 +0100

    Add hasVowels test to test file

--- test/stringClass.test.js
+++ test/stringClass.test.js
@@ -5,12 +5,11 @@ describe("Testing the Extended methods added to String Prototype", function() {
   describe("Testing hasVowels()", function() {
     it("Should return true if the string contains vowels", function() {
       let string = "Vowel is here";
-      expect(string.hasVowels()).toBeTruthy;
+      expect(string.hasVowels()).toBeTruthy();
     });
     it("Should return true if the string contains vowels", function() {
       let string = "gypt";
-      expect(string.hasVowels()).toBeFalsy;
+      expect(string.hasVowels()).toBeFalsy();
     });
   });
-  
 });
