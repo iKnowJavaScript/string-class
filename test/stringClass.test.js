10c5daacf0d4dbbf9888d957d8d884cd6e30e0e3
iKnowJavaScript <remymartins0398@yahoo.com>
2019-05-06 15:31:49 +0100

    Add test file

---
+++ test/stringClass.test.js
@@ -0,0 +1,16 @@
+//Type definitions for Jest
+//npm install @types/jest
+
+describe("Testing the Extended methods added to String Prototype", function() {
+  describe("Testing hasVowels()", function() {
+    it("Should return true if the string contains vowels", function() {
+      let string = "Vowel is here";
+      expect(string.hasVowels()).toBeTruthy;
+    });
+    it("Should return true if the string contains vowels", function() {
+      let string = "gypt";
+      expect(string.hasVowels()).toBeFalsy;
+    });
+  });
+  
+});
